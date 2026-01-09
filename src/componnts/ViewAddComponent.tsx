e46c6df3dc16cfa7de4426bc80a481ad2e716f55
Dinuka Lakmal <Dinuka0512@gmail.com>
2026-01-10 02:04:18 +0530

    Update API endpoint to use production URL

--- src/componnts/ViewAddComponent.tsx
+++ src/componnts/ViewAddComponent.tsx
@@ -28,7 +28,7 @@ export default function ViewAddComponent({ adId }: { adId: string }) {
     const fetchAd = async () => {
       try {
         const res = await axios.get(
-          `http://localhost:5000/api/v1/ads/getAdAllDetails/${adId}`
+          `https://sf-be.vercel.app/api/v1/ads/getAdAllDetails/${adId}`
         );
         setAd(res.data);
         console.log("ad : " + res.data)
